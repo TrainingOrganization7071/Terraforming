a57b625d3ee54dfd001d6a9ba3096fa9130670b1
FouadALLAOUI <fouadallaoui70@gmail.com>
2024-09-26 10:58:28 +0100

    Linux Web App creation

--- main.tf
+++ main.tf
@@ -86,7 +86,6 @@ resource "azurerm_service_plan" "service_plan_web_app" {
   os_type             = var.os_type
   sku_name            = var.sku_value  
   worker_count = var.worker_count
-  reserved            = true
 }
 
 resource "azurerm_linux_web_app" "front_web_app" {
@@ -96,16 +95,17 @@ resource "azurerm_linux_web_app" "front_web_app" {
   location            = azurerm_resource_group.rg.location
   service_plan_id     = azurerm_service_plan.service_plan_web_app.id
   public_network_access_enabled = true
+  
   site_config {
     # Runtime stack for Node.js
-    linux_fx_version = "NODE|14"
+    #linux_fx_version = "NODE|14"
     http2_enabled = true
 
     # Disable always_on to save costs
     always_on = false
 
     # VNET Integration with the App Service Subneta
-    //vnet_route_all_enabled = true
+    #vnet_route_all_enabled = true
   }
   app_settings = {
     WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
