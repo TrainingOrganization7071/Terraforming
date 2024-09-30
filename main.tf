02412ab35c0b83944cef2df53d4d11e40084b509
FouadALLAOUI <fouadallaoui70@gmail.com>
2024-09-30 12:16:13 +0100

    AKS & APIM

--- main.tf
+++ main.tf
@@ -18,6 +18,29 @@ resource "azurerm_subnet" "aks_subnet" {
   address_prefixes     = [ var.aks_subnet_cidr ]
 }
 
+resource "azurerm_network_security_group" "nsg_aks" {
+  name                = "aks-subnet-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-Inbound-NodeJS"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3001"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_association" {
+  subnet_id                 = azurerm_subnet.aks_subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg_aks.id
+}
+
 resource "azurerm_kubernetes_cluster" "aks" {
   depends_on = [ azurerm_subnet.aks_subnet ]
   name                = var.aks_cluster_name
@@ -50,6 +73,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
+
+
+
+
 #az aks get-credentials --resource-group minimal_infra_auth --name aks_back
 
 resource "local_file" "kubeconfig" {
@@ -69,7 +96,7 @@ resource "terraform_data" "localexec" {
 resource "azurerm_subnet" "apim_subnet" {
   name                 = var.apim_subnet_name
   resource_group_name  = var.rg_name
-  virtual_network_name = azurerm_virtual_network.vnet_backend.name
+  virtual_network_name = azurerm_virtual_network.vnet_aks.name
   address_prefixes     = [var.apim_subnet_cidr]
 
   # Required for API Management to be in internal mode
@@ -80,7 +107,7 @@ resource "azurerm_subnet" "apim_subnet" {
     }
   }
 }
-
+/*
 resource "random_string" "randomapim" {
   length           = 16
   special          = false
@@ -90,6 +117,7 @@ resource "random_string" "randomapim" {
 }
 
 resource "azurerm_api_management" "apim" {
+  depends_on = [ azurerm_kubernetes_cluster.aks ]
   name                = "${var.apim_name}-${random_string.randomapim.result}"
   location            = var.rg_location
   resource_group_name = var.rg_name
