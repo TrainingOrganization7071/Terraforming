75f871973014daf45153afb7262bda1b964d3612
FouadALLAOUI <fouadallaoui70@gmail.com>
2024-10-02 09:15:00 +0100

    Structuring the infra files

---
+++ NSG.tf
@@ -0,0 +1,22 @@
+resource "azurerm_network_security_group" "nsg_aks" {
+  name                = "aks-subnet-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-Inbound-NodeJS"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3001"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_association" {
+  subnet_id                 = azurerm_subnet.aks_subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg_aks.id
+}
